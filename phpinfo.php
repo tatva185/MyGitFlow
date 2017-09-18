8e87b0a00cce254156761e21dbcfd8c357a77a57
Sanjay <sanjay.zala@tatvasoft.com>
2017-09-18 16:13:29 +0530

    New changes

--- phpinfo.php
+++ phpinfo.php
@@ -3,4 +3,7 @@
 	echo "This is my hotfix changes";
 	echo "This is my new commit changes";
 	echo "This is my new hotfix files changes";
+	echo "Final commnet hotfix";
+	
+	echo "We are here with HF";
 ?>
